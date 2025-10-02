b1bca63b951a2c0e1a8a283611e4edab193fc921
shevial <sarahevialopez@gmail.com>
2025-10-02 12:55:52 +0200

    isdigit

--- main.c
+++ main.c
@@ -7,7 +7,21 @@ int main(void)
     printf("ft_isalnum('7') = %d (esperado 1)\n", ft_isalnum('7'));
     printf("ft_isalnum('@') = %d (esperado 0)\n", ft_isalnum('@'));
 	printf("ft_isalnum('_') = %d (esperado 0)\n", ft_isalnum('_'));
+    printf("ft_isalnum('a') = %d (esperado 1)\n", ft_isalnum('a'));
 
     return 0;
+
+    // Probar ft_isalpha
+    printf("ft_isalpha('7') = %d (esperado 0)\n", ft_isalpha('7'));
+    printf("ft_isalpha('a') = %d (esperado 1)\n", ft_isalpha('a'));
+    printf("ft_isalpha('&') = %d (esperado 0)\n", ft_isalpha('&'));
+
+    // Probar ft_isdigit
+    printf("ft_isdigit('7') = %d (esperado 1)\n", ft_isdigit('7'));
+    printf("ft_isdigit('a') = %d (esperado 0)\n", ft_isdigit('a'));
+    printf("ft_isdigit('*') = %d (esperado 0)\n", ft_isdigit('*'));
+
+
+
 }
 
